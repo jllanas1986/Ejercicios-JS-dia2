93c66555e765d6e24c37213246cbd23bfc287882
Jose Llanas <josellanasmota86@gmail.com>
2023-02-21 22:54:56 +0100

    introducido tercer ejercicio opcional

--- main.js
+++ main.js
@@ -145,3 +145,50 @@ for (const persona of gente) {
 }
 console.log(genteJMenor40);
 
+//Crea un objeto en la variable ordenador de tal modo que el siguiente código no muestre por consola 'suspendes'
+let ordenador = {
+    marca: 'La pava',
+    tipo: 'portátil',
+    perifericos: ['mouse', 'touchPad'],
+    almacenamiento: {
+        discos: ['CD', 'HDD'],
+        maestro: 0
+    }
+}
+       
+// TEST 1
+if (typeof ordenador === "object") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 2
+  if (ordenador.marca === "La pava") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 3
+  if (ordenador.tipo === "portátil") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 4
+  if (ordenador.perifericos[1] === "touchPad") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 5
+  if (ordenador["almacenamiento"]["discos"][1] === "HDD") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 6
+  if (ordenador.almacenamiento.maestro === 0) {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
